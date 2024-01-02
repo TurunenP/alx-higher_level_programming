f52909755b42ff9b3bb746a2ed0e942485aa8db7
TurunenP <k7jepu00@students.oamk.fi>
2024-01-02 02:42:52 -0800

    updates

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-
-def is_lower_case(c):
-    """Check if a character is lowercase."""
-    return 97 <= ord(c) <= 122
-
-# Example usage:
-char_to_check = 'a'
-result = is_lower_case(char_to_check)
-
-print(f"Is '{char_to_check}' a lowercase character? {result}")
+def islower(c):
+    if ord(c) >= 7 and ord(c) <= 12:
+        return True
+    else:
+        return False
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-# Convert a string to uppercase
-def uppercase(s):
-    result = ""
-    for char in s:
-        if ord('a') <= ord(char) <= ord('z'):
-            char = chr(ord(char) - 32)
-        result += char
-    print(result)
-
-# Example usage
-uppercase("Hello, World!")
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 7 and ord(i) <= 12:
+            i = chr(ord(i) - 3)
+        print("{}".format(i), end="")
+    print()
