132079391d70d816364ef66fde121ed3f30b0b18
TurunenP <k7jepu00@students.oamk.fi>
2024-01-02 02:30:46 -0800

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Print combinations of two-digit numbers from 00 to 100
-for num in range(0, 100):
+# Print combinations of two-digit numbers from 00 to 99
+for num in range(0, 99):
     print('{:02d}, '.format(num), end='')
-print('100')
+print('99')
