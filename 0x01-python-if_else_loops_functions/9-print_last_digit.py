0ff221872f05433b7c6dfcb20bcb34cca0c67ad3
TurunenP <k7jepu00@students.oamk.fi>
2024-01-02 02:48:15 -0800

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    if number < 0:
-        last_digit = number % -(10)
-        print(abs(last_digit), end='')
-    else:
-        last_digit = number % 10
-        print(last_digit, end='')
-    return abs(last_digit)
-
-# Call the function with a sample number
-result = print_last_digit(-123)
-print(" - Absolute value of the last digit:", result)
+    last_digit = abs(number) % 10
+    print(last_digit, end='')
+    return last_digit
